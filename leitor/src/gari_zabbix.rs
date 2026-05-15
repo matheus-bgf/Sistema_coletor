bd3de8ab6883d94fbd2cba4b139432b0e2009594
Matheus Alves <105603566+MamoruSanCD@users.noreply.github.com>
2026-05-15 05:05:01 -0300

    Voltando com código do gari_zabbix

---
+++ leitor/src/gari_zabbix.rs
@@ -0,0 +1,28 @@
+/**
+ *  Bibliotecas necessárias
+ */
+use std::thread;
+use std::time::Duration;
+
+static mut RUNNING: bool = false;
+
+pub fn stop() {
+    println!("zabbix parado");
+}
+
+pub fn start(){
+    unsafe{
+        if RUNNING{
+            return;
+        }
+        RUNNING = true;
+    }
+
+    thread::spawn(|| {
+        println!("Gari zabbix apostos");
+
+        loop {
+            thread::sleep(std::time::Duration::from_secs(1));
+        }
+    });
+}
